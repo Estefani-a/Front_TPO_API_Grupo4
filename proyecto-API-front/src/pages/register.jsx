8cbd92922459ed030a869bd36b6a5e260bc57709
Lola Diaz <137005124+loladiaz1@users.noreply.github.com>
2025-09-12 03:56:38 -0300

    arreglo

--- proyecto-API-front/src/pages/register.jsx
+++ proyecto-API-front/src/pages/register.jsx
@@ -8,38 +8,38 @@ export default function register() {
         <div className="logo">
           <div className="logo-placeholder">LOGO</div>
         </div>
-
+        
         <h2>Registrarse</h2>
-
+        
         <form>
           <div className="form-group">
             <label>Nombre</label>
             <input type="text" placeholder="Tu nombre" />
           </div>
-
+          
           <div className="form-group">
             <label>Email</label>
             <input type="email" placeholder="correo@ejemplo.com" />
           </div>
-
+          
           <div className="form-group">
             <label>Contraseña</label>
             <input type="password" placeholder="********" />
           </div>
-
+          
           <div className="form-group">
             <label>Confirmar Contraseña</label>
             <input type="password" placeholder="********" />
           </div>
-
+          
           <button type="submit" className="btn">
             Crear cuenta
           </button>
         </form>
-
+        
         <p className="switch-text">
           ¿Ya tienes cuenta?{" "}
-          <Link to="/" className="link">
+          <Link to="/login" className="link">
             Inicia sesión
           </Link>
         </p>
